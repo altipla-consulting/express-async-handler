dc75e78286497794477a14f76a9f71ba404f00bb
Marisa Robayo Moreno <marisarobayo@gmail.com>
2023-04-05 10:08:42 +0200

    Implement handler.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 
+import type { NextFunction, Request, Response } from 'express'
+
+export function asyncHandler<T>(handler: (req: Request, resp: Response, next: NextFunction) => Promise<T> | T) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(handler(req, res, next))
+      .then(result => {
+        if (result) {
+          res.send(result)
+        }
+      })
+      .catch(next)
+  }
+}
